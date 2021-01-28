244462eda57058082833ba2506a3ba40aaff19a2
Fernanda Rosa <ferosa91@gmail.com>
2021-01-28 14:32:59 +0100

    Final

---
+++ js/index-game.js
@@ -0,0 +1,18 @@
+// let ptbrButton = document.getElementById("ptbr-button");
+// let engButton = document.getElementById("eng-button");
+
+// ptbrButton.onclick = 
+// function buttonSoundPtbr() {
+//     var audioPtbr = new Audio("./ptbr-sounds/01enter-cafe-audio.mp3");
+//     audioPtbr.play();
+//     audioPtbr.volume = 0.2;
+// }
+
+// engButton.onclick = 
+// function buttonSoundEng() {
+//   var audioEng = new Audio("./eng-sounds/01enter-pessimistic.mp3");
+//   audioEng.play();
+//   audioEng.volume = 0.2;
+// }
+
+
