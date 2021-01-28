31eab66f279afa7f2032bc5221d9d03cf5716eac
Fernanda Rosa <ferosa91@gmail.com>
2021-01-28 16:27:49 +0100

    with corrected audio

--- js/index-game.js
+++ js/index-game.js
@@ -1,20 +1,18 @@
 let ptbrButtonLink = document.getElementById("ptbr-button-link");
 let engButtonLink = document.getElementById("eng-button-link");
 
-ptbrButtonLink.onclick = 
-function buttonSoundPtbr(event) {
-    event.preventDefault()
-    var audioPtbr = new Audio("./ptbr-sounds/01enter-cafe-audio.mp3");
-    audioPtbr.volume = 0.2;
-    audioPtbr.play();
-    setTimeout(() => window.location.href="./ptbr-game.html", 2000);
-}
+ptbrButtonLink.onclick = function buttonSoundPtbr(event) {
+  event.preventDefault();
+  var audioPtbr = new Audio("./ptbr-sounds/01enter-cafe-audio.mp3");
+  audioPtbr.volume = 0.2;
+  audioPtbr.play();
+  setTimeout(() => (window.location.href = "./ptbr-game.html"), 2000);
+};
 
-engButtonLink.onclick = 
-function buttonSoundEng(event) {
-    event.preventDefault()
+engButtonLink.onclick = function buttonSoundEng(event) {
+  event.preventDefault();
   var audioEng = new Audio("./eng-sounds/01enter-pessimistic.mp3");
   audioEng.volume = 0.2;
   audioEng.play();
-  setTimeout(() => window.location.href="./ptbr-game.html", 2000);
-}
+  setTimeout(() => (window.location.href = "./eng-game.html"), 2000);
+};
